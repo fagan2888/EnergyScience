80558ca4d4a6a1afa7c44d3640fb90d5b3f9e7fc
Nishtala, Raj <rnishtala@sciencelogic.com>
2019-04-16 00:55:20 -0400

    Include holiday codes and merge all CSVs

--- src/data_ingest.py
+++ src/data_ingest.py
@@ -18,7 +18,9 @@ from db_functions import create_dataset_tables, insert_rows
 SCRIPT_PATH = os.path.dirname(__file__)  # Script directory
 HOLIDAY_READ_PATH = os.path.join(SCRIPT_PATH, '../data/raw/power-laws-forecasting-energy-consumption-holidays.csv')
 WEATHER_READ_PATH = os.path.join(SCRIPT_PATH, '../data/raw/power-laws-forecasting-energy-consumption-weather.csv')
-WRITE_PATH = os.path.join(SCRIPT_PATH, '../data/interim')
+TRAINING_READ_PATH = os.path.join(SCRIPT_PATH, '../data/raw/power-laws-forecasting-energy-consumption-training-data.csv')
+# WEATHER_READ_PATH = os.path.join(SCRIPT_PATH, '../data/interim/site-1-weather.csv')
+WRITE_PATH = os.path.join(SCRIPT_PATH, '../data/interim/')
 
 class DataIngest(object):
     """
@@ -32,7 +34,24 @@ class DataIngest(object):
         self.count_interim_files = 0
         self.data_holidays = None
         self.data_weather = None
+        self.training_data = None
+        self.dataset = None
+        self.filenames = []
         self.connection = None
+        self.holiday_codes = {'Christmas Day': 0, 'Columbus Day': 1,
+                              'Fraternal Day': 1, 'American Indian Heritage Day': 1,
+                              'Veterans Day': 2, 'Confederate Memorial Day': 2,
+                              'Memorial Day': 3, 'Labor Day': 4, 'George Washington': 5,
+                              "Washington's Birthday": 5, 'Christmas Eve (Observed)': 13,
+                              'Thomas Jefferson Birthday': 5, 'Robert E. Lee': 6,
+                              'Martin Luther King Birthday': 6, 'Independence Day': 7,
+                              'Thanksgiving Day': 8, 'Jefferson Davis Birthday': 9,
+                              'Christmas Day (Observed)': 10, 'New year': 11,
+                              'Independence Day (Observed)': 12, 'Election Day': 14,
+                              'New Years Eve (Observed)': 15, 'New Years Eve': 16,
+                              "Birthday of Martin Luther King, Jr.": 6,
+                              'New Years Eve Shift': 17, 'Christmas Eve shift': 18,
+                              'Mardi Gras': 19, 'Good Friday': 20}
 
     def connect_to_database(self, schema):
         self.connection = mysql.connector.connect(host='35.227.50.121',
@@ -47,6 +66,12 @@ class DataIngest(object):
         date_as_string = str(date_in_some_format)
         year_as_string = date_as_string.split('T')[0] # last four characters
         return year_as_string
+
+    def get_holiday_code(self, holiday):
+        """
+        :param holiday:
+        """
+        return self.holiday_codes.get(holiday, -1)
 
     def generate_file_name(self, site_id, data_type):
         return "site-{}-{}.csv".format(site_id, data_type)
@@ -65,44 +90,66 @@ class DataIngest(object):
         Ingest from csv
         :param site_id: The Site ID
         """
-        self.connect_to_database('esschema')
-        create_dataset_tables(self.connection)
+        #self.connect_to_database('esschema')
+        #create_dataset_tables(self.connection)
         self.data_holidays = pd.read_csv(HOLIDAY_READ_PATH, sep=';')
         # data_holiday_headers = ','.join(list(self.data_holidays.columns))
         data_holiday_rows = [tuple(row) for row in self.data_holidays.values]
-        insert_rows(self.connection, 'esschema', 'holiday1', 'Holiday_date, Holiday, SiteId',
-                    data_holiday_rows, '%s, %s, %s', logger=self.logger)
+        #insert_rows(self.connection, 'esschema', 'holiday1', 'Holiday_date, Holiday, SiteId',
+        #            data_holiday_rows, '%s, %s, %s', logger=self.logger)
         self.data_weather = pd.read_csv(WEATHER_READ_PATH, sep=';')
-        data_weather_headers = ','.join(list(self.data_weather.columns))
-        self.logger.debug(data_weather_headers)
+        #data_weather_headers = ','.join(list(self.data_weather.columns))
         weather_data_rows = [tuple(row) for row in self.data_weather.values]
-        insert_rows(self.connection, 'esschema', 'weather1', data_weather_headers,
-                    weather_data_rows, '%s, %s, %s, %s', logger=self.logger)
-        self.connection.close()
+        self.training_data = pd.read_csv(TRAINING_READ_PATH, sep=";")
+        training_data_rows = [tuple(row) for row in self.training_data.values]
+        # self.logger.debug(weather_data_rows)
+        #insert_rows(self.connection, 'esschema', 'weather1', 'Timestamp,Temperature,Distance,SiteId',
+        #            weather_data_rows, '%s, %s, %s, %s', logger=self.logger)
+        #self.connection.close()
 
 
     def wrangle(self):
         """
         Wrangle holiday and weather datasets
         """
-        for site in range (0, 2):
+        for site in range (0, 350):
             holiday_data_by_site = self.data_holidays[self.data_holidays['SiteId'] == site]
             weather_data_by_site = self.data_weather[self.data_weather['SiteId'] == site]
-            self.logger.debug("Adding the date column to weather dataset")
-            if not holiday_data_by_site.empty and not weather_data_by_site.empty:
+            training_data_by_site = self.training_data[self.training_data['SiteId'] == site]
+            if not holiday_data_by_site.empty and not weather_data_by_site.empty and not training_data_by_site.empty:
+                self.logger.info("Wrangling Site ID {}".format(site))
                 weather_data_by_site['Date'] = weather_data_by_site['Timestamp'].apply(self.convert_to_date)
+                training_data_by_site['Date'] = training_data_by_site['Timestamp']
                 merged_dataset = pd.merge(holiday_data_by_site, weather_data_by_site,
                                           on='Date', how='outer')
-                self.count_interim_files+=1
-                self.logger.debug('making intertim data set from raw data')
-                self.logger.debug(holiday_data_by_site)
-                self.logger.debug(weather_data_by_site)
+                self.dataset = pd.merge(merged_dataset, training_data_by_site,
+                                        on='Date', how='outer')
+                self.dataset = self.dataset[pd.isna(self.dataset['Value']) == False]
+                self.dataset['HolidayCode'] = self.dataset['Holiday'].apply(self.get_holiday_code)
                 holiday_file_name = self.generate_file_name(site, 'holiday')
                 weather_file_name = self.generate_file_name(site, 'weather')
-                merged_file_name = self.generate_file_name(site, 'weather-holiday')
+                training_file_name = self.generate_file_name(site, 'training')
+                merged_file_name = self.generate_file_name(site, 'training-weather-holiday')
                 self.write_records_to_file(holiday_data_by_site, holiday_file_name)
                 self.write_records_to_file(weather_data_by_site, weather_file_name)
-                self.write_records_to_file(merged_dataset, merged_file_name)
+                self.write_records_to_file(training_data_by_site, training_file_name)
+                self.write_records_to_file(self.dataset, merged_file_name)
+
+    def merge_csvs(self):
+        """
+        Merge all CSVs
+        """
+        site_ids  = []
+        for site in range(0, 350):
+            filename = self.generate_file_name(site, 'training-weather-holiday')
+            if os.path.isfile(WRITE_PATH+filename):
+                self.count_interim_files+=1
+                site_ids.append(site)
+                self.filenames.append(filename)
+        # self.logger.debug(site_ids)
+        # self.logger.debug(self.filenames)
+        combined_csv = pd.concat([pd.read_csv(WRITE_PATH+f, sep=',') for f in self.filenames])
+        combined_csv.to_csv(WRITE_PATH+"site-all-training-weather-holiday.csv", index=False)
 
     @property
     def sites_count(self):
@@ -114,5 +161,6 @@ if __name__ == '__main__':
     logger = logging.getLogger(__name__)
     data_ingest = DataIngest(logger=logger)
     data_ingest.ingest()
-    #data_ingest.wrangle()
+    data_ingest.wrangle()
+    data_ingest.merge_csvs()
     logger.info("Sites count: {}".format(data_ingest.sites_count))
